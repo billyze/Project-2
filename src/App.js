62794c1454612a9e6b57f98ab9576da5214c27cc
unknown <billyzedan123@gmail.com>
2020-04-27 16:17:39 -0400

    starter code for project 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// import { Switch, Route } from 'react-router-dom'
+import Axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let api_key = "9W5UPEGYZVUVW53C"
+
+class App extends Component {
+  
+  state = {
+    name: '',
+    image: [],
+    company: {},
+    imageExists: false
+  }
+
+  componentDidMount = () => {
+    Axios.get("https://cors-anywhere.herokuapp.com/https://random.dog/woof.json")
+    .then(response => {
+      console.log(response)
+      this.setState({image:response.data, imageExists:true})
+    })
+    Axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=${api_key}`)
+    .then(response => {
+      console.log(response.data)
+      // this.setState({
+      //   company: {
+      //     name: response.data.Meta_Data
+      //   }
+      // })
+    })
+    Axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=AAPL&apikey=${api_key}`)
+    .then(response => {
+      console.log(response)
+      this.setState({
+        name: response.data.bestMatches[0]['2. name']
+      })
+    })
+  }
+
+  setImage = () => {
+    if(this.state.imageExists)
+    {
+      return <div><img width="500px" height="500px" alt="oops!" src={this.state.image.url} /></div>
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.setImage()}
+        {this.state.name}
+      </div>
+    );
+  }
 }
 
 export default App;
