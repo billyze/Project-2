0d25c167c9b31a07f715492e64e7f97ab1278bc9
unknown <billyzedan123@gmail.com>
2020-05-01 11:33:44 -0400

    more graphs

--- src/Components/company/Company.js
+++ src/Components/company/Company.js
@@ -15,24 +15,24 @@ class Company extends Component {
     }
 
     componentDidMount = () => {
-        let dataCopy = []
-        let dateCopy = []
-        let index = 0
-        Axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${this.props.match.params.companySymbol}&interval=${this.state.Interval}&apikey=${api_key}`)
-        .then(response => {
-          for(var key in response.data[`Time Series (${this.state.Interval})`])
-          {
-              dateCopy[index] = key
-              dataCopy[index] = response.data[`Time Series (${this.state.Interval})`][key]['4. close']
-              index++
-          }
-          this.setState({
-                Symbol: response.data['Meta Data']['2. Symbol'],
-                load: true,
-                data: dataCopy,
-                date: dateCopy
-            })
-          })
+        // let dataCopy = []
+        // let dateCopy = []
+        // let index = 0
+        // Axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${this.props.match.params.companySymbol}&interval=${this.state.Interval}&apikey=${api_key}`)
+        // .then(response => {
+        //   for(var key in response.data[`Time Series (${this.state.Interval})`])
+        //   {
+        //       dateCopy[index] = key
+        //       dataCopy[index] = response.data[`Time Series (${this.state.Interval})`][key]['4. close']
+        //       index++
+        //   }
+        //   this.setState({
+        //         Symbol: response.data['Meta Data']['2. Symbol'],
+        //         load: true,
+        //         data: dataCopy,
+        //         date: dateCopy
+        //     })
+        //   })
         }
 
     displayData = () => {
@@ -40,10 +40,7 @@ class Company extends Component {
         console.log(x)
         return (
             <div>
-                {this.state.Symbol}: {this.state.data[0]}
-                {console.log(this.state.data)}
-                {console.log(this.state.date)}
-                <Canvas data={this.state.data} date={this.state.date} companyName={this.props.match.params.companySymbol} />
+                <Canvas companyName={this.props.match.params.companySymbol} />
             </div>
         )
     }
@@ -51,9 +48,7 @@ class Company extends Component {
     render() {
         return (
             <div>
-                {console.log(this.props)}
-                {console.log(this.props.match.params.companySymbol)}
-                {this.state.load? (this.displayData()) : ('')}
+                {this.displayData()}
             </div>
         );
     }
